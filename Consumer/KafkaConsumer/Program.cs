e2fcae03c508a491f35e97c332f8a6ddc7d6e064
semaleksandrov <semaleksandrov@ozon.ru>
2023-08-28 18:10:26 +0300

    example consumer and producer

--- Consumer/KafkaConsumer/Program.cs
+++ Consumer/KafkaConsumer/Program.cs
@@ -1,39 +1,16 @@
-﻿// Copyright 2016-2018 Confluent Inc., 2015-2016 Andreas Heider
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// Derived from: rdkafka-dotnet, licensed under the 2-clause BSD License.
-//
-// Refer to LICENSE for more information.
+﻿using Confluent.Kafka;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-
-
-/// <summary>
-///     Demonstrates use of the Consumer client.
-/// </summary>
-namespace Confluent.Kafka.Examples.ConsumerExample
+namespace KafkaConsumer
 {
     public class Program
     {
-        /// <summary>
-        ///     In this example
-        ///         - offsets are automatically committed.
-        ///         - no extra thread is created for the Poll (Consume) loop.
-        /// </summary>
+
+        public static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Run_Consume("localhost:9092", new List<string>() { "test-topic" }, cts.Token);
+        }
+
         public static void Run_Consume(string brokerList, List<string> topics, CancellationToken cancellationToken)
         {
             var config = new ConsumerConfig
@@ -116,6 +93,7 @@ namespace Confluent.Kafka.Examples.ConsumerExample
                             Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
                             try
                             {
+                                Console.WriteLine($"Value {consumeResult.Message.Value}");
                                 // Store the offset associated with consumeResult to a local cache. Stored offsets are committed to Kafka by a background thread every AutoCommitIntervalMs.
                                 // The offset stored is actually the offset of the consumeResult + 1 since by convention, committed offsets specify the next message to consume.
                                 // If EnableAutoOffsetStore had been set to the default value true, the .NET client would automatically store offsets immediately prior to delivering messages to the application.
@@ -141,94 +119,5 @@ namespace Confluent.Kafka.Examples.ConsumerExample
             }
         }
 
-        /// <summary>
-        ///     In this example
-        ///         - consumer group functionality (i.e. .Subscribe + offset commits) is not used.
-        ///         - the consumer is manually assigned to a partition and always starts consumption
-        ///           from a specific offset (0).
-        /// </summary>
-        public static void Run_ManualAssign(string brokerList, List<string> topics, CancellationToken cancellationToken)
-        {
-            var config = new ConsumerConfig
-            {
-                // the group.id property must be specified when creating a consumer, even
-                // if you do not intend to use any consumer group functionality.
-                GroupId = "groupid-not-used-but-mandatory",
-                BootstrapServers = brokerList,
-                // partition offsets can be committed to a group even by consumers not
-                // subscribed to the group. in this example, auto commit is disabled
-                // to prevent this from occurring.
-                EnableAutoCommit = false
-            };
-
-            using (var consumer =
-                new ConsumerBuilder<Ignore, string>(config)
-                    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
-                    .Build())
-            {
-                consumer.Assign(topics.Select(topic => new TopicPartitionOffset(topic, 0, Offset.Beginning)).ToList());
-
-                try
-                {
-                    while (true)
-                    {
-                        try
-                        {
-                            var consumeResult = consumer.Consume(cancellationToken);
-                            // Note: End of partition notification has not been enabled, so
-                            // it is guaranteed that the ConsumeResult instance corresponds
-                            // to a Message, and not a PartitionEOF event.
-                            Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: ${consumeResult.Message.Value}");
-                        }
-                        catch (ConsumeException e)
-                        {
-                            Console.WriteLine($"Consume error: {e.Error.Reason}");
-                        }
-                    }
-                }
-                catch (OperationCanceledException)
-                {
-                    Console.WriteLine("Closing consumer.");
-                    consumer.Close();
-                }
-            }
-        }
-
-        private static void PrintUsage()
-            => Console.WriteLine("Usage: .. <subscribe|manual> <broker,broker,..> <topic> [topic..]");
-
-        public static void Main(string[] args)
-        {
-            if (args.Length < 3)
-            {
-                PrintUsage();
-                return;
-            }
-
-            var mode = args[0];
-            var brokerList = args[1];
-            var topics = args.Skip(2).ToList();
-
-            Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
-
-            CancellationTokenSource cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) => {
-                e.Cancel = true; // prevent the process from terminating.
-                cts.Cancel();
-            };
-
-            switch (mode)
-            {
-                case "subscribe":
-                    Run_Consume(brokerList, topics, cts.Token);
-                    break;
-                case "manual":
-                    Run_ManualAssign(brokerList, topics, cts.Token);
-                    break;
-                default:
-                    PrintUsage();
-                    break;
-            }
-        }
     }
 }
